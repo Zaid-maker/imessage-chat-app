e7541ec981f15be921e29fcbf5b9a668432d9c78
Zaid-maker <pzhafeez@gmail.com>
2023-01-07 00:34:29 +0500

    update home

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage, NextPageContext } from "next";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
 
   return <div>Home</div>;
 };
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
 
 export default Home;
