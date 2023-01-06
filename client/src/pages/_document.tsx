d27dbb74bcb0f2eec83ea4a286c3485086d3c1ec
Zaid-maker <pzhafeez@gmail.com>
2023-01-06 14:46:07 +0500

    update document and home page

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          {/*<ColorModeScript initialColorMode={theme.config.initialColorMode} />*/}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Hello Home Pgae</div>;
-}
+import type { NextPage, NextPageContext } from "next";
+import { useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data: session } = useSession();
+
+  return <div>Home</div>;
+};
+
+export default Home;
