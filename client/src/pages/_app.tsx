6f2459af1d6d5319281d811bf5481d5efc262f6b
Zaid-maker <pzhafeez@gmail.com>
2023-01-07 19:12:38 +0500

    wrap the app with chakra

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
@@ -7,7 +8,9 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <ChakraProvider>
+        <Component {...pageProps} />;
+      </ChakraProvider>
     </SessionProvider>
   );
 }
