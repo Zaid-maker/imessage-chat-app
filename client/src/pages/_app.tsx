ec0b12053206d9780c3b02653e88fcb41df22517
Zaid-maker <pzhafeez@gmail.com>
2023-01-09 23:12:28 +0500

    wrap apollo client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,23 @@
+import { ApolloClient } from "@apollo/client/core";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { theme } from "../chakra/theme";
+import { client } from "../graphql/apollo-client";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-        <Toaster />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloClient client={client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+          <Toaster />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloClient>
   );
 }
