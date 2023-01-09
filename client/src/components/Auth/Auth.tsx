50b279439a0f4e089ebf38095bf5e5e58290cf27
Zaid-maker <pzhafeez@gmail.com>
2023-01-09 13:54:17 +0500

    complete the Auth

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -1,6 +1,7 @@
-import { Center, Stack, Text } from "@chakra-ui/react";
+import { Button, Center, Image, Input, Stack, Text } from "@chakra-ui/react";
 import { Session } from "next-auth";
-import React, { FC } from "react";
+import { signIn } from "next-auth/react";
+import { ChangeEvent, FC, useState } from "react";
 
 interface AuthProps {
   session: Session | null;
@@ -8,10 +9,45 @@ interface AuthProps {
 }
 
 const Auth: FC<AuthProps> = ({ session, reloadSession }) => {
+  const [username, setUsername] = useState("");
+
   return (
     <Center height="100vh">
       <Stack spacing={8} align="center">
-        <Text>Sign in with your favorite provider and start chatting!</Text>
+        {session ? (
+          <>
+            <Text fontSize="3xl">Create a Username</Text>
+            <Input
+              placeholder="Enter a Username"
+              value={username}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setUsername(event.target.value)
+              }
+            />
+            <Button width="100%">Save</Button>
+          </>
+        ) : (
+          <>
+            <Image alt="logo" height={100} src="/images/imessage-logo.png" />
+            <Text fontSize="4xl">Single&apos;s Chat 😜</Text>
+            <Text width="70%" align="center">
+              Sign in with Google to send unlimited free messages to your
+              friends.
+            </Text>
+            <Button
+              onClick={() => signIn("google")}
+              leftIcon={
+                <Image
+                  src="/images/googlelogo.png"
+                  height="20px"
+                  alt="googlelogo"
+                />
+              }
+            >
+              Continue with Google
+            </Button>
+          </>
+        )}
       </Stack>
     </Center>
   );
