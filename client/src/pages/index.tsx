3d2cecaf395c97b7de5130ff2e2d13d85e121527
Zaid-maker <pzhafeez@gmail.com>
2023-01-09 22:02:28 +0500

    context and update home

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage, NextPageContext } from "next";
 import { getSession, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Auth from "../components/Auth/Auth";
 import Chat from "../components/Chat";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  const router = useRouter();
 
   /**
    * It creates a new event, dispatches it, and then returns nothing
@@ -15,14 +18,18 @@ const Home: NextPage = () => {
     document.dispatchEvent(event);
   };
 
+  useEffect(() => {
+    if (!session?.user && router.query.conversationId) {
+      router.replace(process.env.NEXT_PUBLIC_BASE_URL as string);
+    }
+  }, [session?.user, router.query, router]);
+
   return (
     <Box>
       {session && session?.user?.username ? (
         <Chat session={session} />
       ) : (
-        <>
-          <Auth session={session} reloadSession={reloadSession} />
-        </>
+        <Auth session={session} reloadSession={reloadSession} />
       )}
     </Box>
   );
