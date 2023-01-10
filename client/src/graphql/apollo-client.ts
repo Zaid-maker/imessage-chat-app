343820e52829f3daf3573275d2d4eab109764cec
Zaid-maker <pzhafeez@gmail.com>
2023-01-10 21:39:38 +0500

    fix

--- client/src/graphql/apollo-client.ts
+++ client/src/graphql/apollo-client.ts
@@ -4,27 +4,23 @@ import { getMainDefinition } from "@apollo/client/utilities";
 import { createClient } from "graphql-ws";
 import { getSession } from "next-auth/react";
 
-/* Creating a new GraphQLWsLink object. */
 const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
           url: "ws://localhost:4000/graphql/subscriptions",
           connectionParams: async () => ({
-            session: getSession,
+            session: await getSession(),
           }),
         })
       )
     : null;
 
-/* Creating a new HttpLink object. */
 const httpLink = new HttpLink({
   uri: `http://localhost:4000/graphql`,
   credentials: "include",
 });
 
-/* Checking if the window object is defined and if the wsLink object is not null. If both are true, it
-will return the split function. If not, it will return the httpLink object. */
 const link =
   typeof window !== "undefined" && wsLink != null
     ? split(
@@ -40,7 +36,6 @@ const link =
       )
     : httpLink;
 
-/* Exporting the ApolloClient object. */
 export const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
