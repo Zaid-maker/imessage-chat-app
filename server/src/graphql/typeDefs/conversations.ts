75e489c2fe13ef29d8d49214c8d20d5280abc552
Zaid-maker <pzhafeez@gmail.com>
2023-01-11 20:02:31 +0500

    conversation type

--- server/src/graphql/typeDefs/conversations.ts
+++ server/src/graphql/typeDefs/conversations.ts
@@ -4,7 +4,7 @@ const typeDefs = gql`
   type Conversation {
     id: String
     latestMessage: Message
-    participants: [participant]
+    participants: [Participant]
     updatedAt: Date
   }
 
@@ -12,6 +12,55 @@ const typeDefs = gql`
     id: String
     user: User
     hasSeenLatestMessage: Boolean
+  }
+
+  type CreateConversationResponse {
+    conversationId: String
+  }
+
+  type ConversationDeletedResponse {
+    id: String
+  }
+
+  type ConversationUpdatedSubscriptionPayload {
+    conversation: Conversation
+    addedUserIds: [String]
+    removedUserIds: [String]
+  }
+
+  type Query {
+    conversations: [Conversation]
+  }
+
+  type Mutation {
+    createConversation(participantIds: [String]): CreateConversationResponse
+  }
+
+  type Mutation {
+    markConversationAsRead(userId: String!, conversationId: String!): Boolean
+  }
+
+  type Mutation {
+    deleteConversation(conversationId: String!): Boolean
+  }
+
+  type Mutation {
+    updateParticipants(
+      conversationId: String!
+      participantIds: [String]!
+    ): Boolean
+  }
+
+  type Subscription {
+    conversationCreated: Conversation
+  }
+
+  type Subscription {
+    conversationUpdated: ConversationUpdatedSubscriptionPayload
+  }
+
+  type Subscription {
+    conversationDeleted: ConversationDeletedResponse
   }
 `;
 
