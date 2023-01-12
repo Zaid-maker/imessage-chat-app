ae7a0d6c397dff4950efc637f1b89b6d1972ae31
Zaid-maker <pzhafeez@gmail.com>
2023-01-12 22:56:48 +0500

    scalar

--- server/src/graphql/resolvers/scalars.ts
+++ server/src/graphql/resolvers/scalars.ts
@@ -1,3 +1,25 @@
-const resolvers = {};
+import { GraphQLScalarType, Kind } from "graphql";
+
+const dateScalar = new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value: any) {
+    return value.getTime();
+  },
+  parseValue(value: any) {
+    return new Date(value);
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10));
+    }
+
+    return null;
+  },
+});
+
+const resolvers = {
+  Date: dateScalar,
+};
 
 export default resolvers;
