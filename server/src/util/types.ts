7985fcc90a4bf6b2eeb179868941876b3d2024dc
Zaid-maker <pzhafeez@gmail.com>
2023-01-20 16:09:24 +0500

    complete the types

--- server/src/util/types.ts
+++ server/src/util/types.ts
@@ -1,8 +1,15 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { PubSub } from "graphql-subscriptions";
 import { Context } from "graphql-ws/lib/server";
-import { participantPopulated } from "../graphql/resolvers/conversations";
+import {
+  conversationPopulated,
+  participantPopulated,
+} from "../graphql/resolvers/conversations";
+import { messagePopulated } from "../graphql/resolvers/messages";
 
+/**
+ * Server Configuration
+ */
 export interface Session {
   user?: User;
 }
@@ -19,6 +26,9 @@ export interface SubscriptionContext extends Context {
   };
 }
 
+/**
+ * Users Types
+ */
 export interface User {
   id: string;
   username: string;
@@ -29,10 +39,13 @@ export interface CreateUsernameResponse {
   error?: string;
 }
 
-/**
- * Messages
- */
+export interface SearchUsersResponse {
+  users: Array<User>;
+}
 
+/**
+ * Messages Types
+ */
 export interface SendMessageArguments {
   id: string;
   conversationId: string;
@@ -40,10 +53,37 @@ export interface SendMessageArguments {
   body: string;
 }
 
+export interface SendMessageSubscriptionPayload {
+  messageSent: MessagePopulated;
+}
+
+export type MessagePopulated = Prisma.MessageGetPayload<{
+  include: typeof messagePopulated;
+}>;
+
 /**
  * Conversations
  */
+export type ConversationPopulated = Prisma.ConversationGetPayload<{
+  include: typeof conversationPopulated;
+}>;
 
 export type ParticipantPopulated = Prisma.ConversationParticipantGetPayload<{
   include: typeof participantPopulated;
 }>;
+
+export interface ConversationCreatedSubscriptionPayload {
+  conversationCreated: ConversationPopulated;
+}
+
+export interface ConversationUpdatedSubscriptionData {
+  conversationUpdated: {
+    conversation: ConversationPopulated;
+    addedUserIds: Array<string>;
+    removedUserIds: Array<string>;
+  };
+}
+
+export interface ConversationDeletedSubscriptionPayload {
+  conversationDeleted: ConversationPopulated;
+}
